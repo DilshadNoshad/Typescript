4dd5884796feaf37bcc7cd8184fd3432cbde6fe7
DilshadSheikh <dilshadnoshad2003@gmail.com>
2024-04-23 16:47:42 +0500

    first commit

---
+++ 01 - Initialize A TypeScript Project Manually/script.js
@@ -0,0 +1,6 @@
+var num1 = 2;
+var num2 = 3;
+function sum(x, y) {
+    return x + y;
+}
+sum(3);
---
+++ 01 - Initialize A TypeScript Project Manually/script.ts
@@ -0,0 +1,6 @@
+const num1 = 2;
+const num2 = 3;
+
+function sum(x: number, y: number) {
+  return x + y;
+}
